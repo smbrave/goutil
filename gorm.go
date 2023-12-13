4562d6124ff9cf0dc2b18eb68ecf3e6ab38c3cff
jiangyong27 <yong27@163.com>
2023-12-13 11:58:51 +0800

    model column

--- gorm.go
+++ gorm.go
@@ -5,6 +5,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"reflect"
 	"time"
 )
 
@@ -40,4 +41,29 @@ func (d *GORMLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql 
 			log.Debugf("[SQL]sql=%s affect=%d cost=%dms", sql, affects, time.Since(begin).Milliseconds())
 		}
 	}
+}
+
+func ModelInitColumn(md interface{}, col interface{}) {
+	modelValue := reflect.TypeOf(md).Elem()
+	modelCol := reflect.ValueOf(col).Elem()
+	for i := 0; i < modelValue.NumField(); i++ {
+		field := modelValue.Field(i)
+		fValue := modelCol.FieldByName(field.Name)
+		if fValue.IsValid() && fValue.CanSet() {
+			fValue.Set(reflect.ValueOf(SnakeString(field.Name)))
+		}
+	}
+}
+
+func ModelFieldValue(md interface{}, fields ...string) map[string]interface{} {
+	data := make(map[string]interface{})
+	obj := reflect.ValueOf(md).Elem()
+
+	for _, f := range fields {
+		val := obj.FieldByName(CamelString(f))
+		if val.IsValid() {
+			data[f] = val.Interface()
+		}
+	}
+	return data
 }
