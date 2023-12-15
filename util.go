2064d0df6a9a0024b51ab6242a35596c95f7f688
jiangyong27 <yong27@163.com>
2023-12-15 11:50:26 +0800

    FormatBytes

--- util.go
+++ util.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 // format bytes number friendly
@@ -74,4 +75,17 @@ func CopyStruct(dst interface{}, src interface{}) {
 		}
 	}
 
+}
+
+func FormatBytes(bytes int64) string {
+	const unit = 1024
+	if bytes < unit {
+		return strconv.FormatInt(bytes, 10) + " B"
+	}
+	div, exp := int64(unit), 0
+	for n := bytes / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.2f%cB", float64(bytes)/float64(div), "KMGTPE"[exp])
 }
